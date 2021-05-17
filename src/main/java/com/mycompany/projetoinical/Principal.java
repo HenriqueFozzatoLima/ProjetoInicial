dab434704c94f7228171a0c7d741f6c69467a7d0
HenriqueFozzatoLima <84341840+HenriqueFozzatoLima@users.noreply.github.com>
2021-05-17 14:44:38 -0300

    Adicionado Olá Mundo

--- src/main/java/com/mycompany/projetoinical/Principal.java
+++ src/main/java/com/mycompany/projetoinical/Principal.java
@@ -12,29 +12,10 @@ package com.mycompany.projetoinical;
 public class Principal {
     public static void main(String[] args) {
         
-        
-        
-        
-        
-        
-        
-        
-        
+        System.out.println("Olá Mundo!");
+       
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
